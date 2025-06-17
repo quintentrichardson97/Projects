abf61d1b039843b066a3c13d1d29fbf0fe7024d1
Quinten Richardson <qwebdevelopment@gmail.com>
2025-06-17 00:53:32 -0500

    requirements.txt added

--- login/views.py
+++ login/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import SignupForm, LoginForm
 
 # Create your views here.
+def login_view(request):
+    form = SignupForm()
+    return render(request, 'signup.html', {'form': form})
